d4d72ab1bb63359080d74805ffe30d46010e4f1f
AlanAlv <alan.alv96@gmail.com>
2020-03-18 01:17:38 -0600

    Get words

--- app/src/main/java/com/example/wtww/MainActivity.java
+++ app/src/main/java/com/example/wtww/MainActivity.java
@@ -3,12 +3,28 @@ package com.example.wtww;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView wordTxt;
+    TextView wordMeaningTxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        wordTxt = findViewById(R.id.wordNameView);
+        wordMeaningTxt = findViewById(R.id.wordMeaningView);
+
+        wordTxt.setText("Smonday");
+        wordMeaningTxt.setText("The moment when Sunday stops feeling like a Sunday and the anxiety of Monday kicks in.");
+
+    }
+
+    public void displayWeirdWord(View view) {
+        String[] word = weirdWordDeck.shuffleAndPick();
+        wordTxt.setText(word[0]);
+        wordMeaningTxt.setText(word[1]);
     }
 }
---
+++ app/src/main/java/com/example/wtww/weirdWordDeck.java
@@ -0,0 +1,31 @@
+package com.example.wtww;
+
+
+public class weirdWordDeck {
+
+    private static final String[] weirdWord = {
+
+            "Flawsome",
+            "Fudgel",
+            "Unkeyboardinated",
+            "Rawgabbit",
+            "Abbiocco",
+            "Textpectation"
+    };
+    private static final String[] weirdMeaning = {
+
+            "An individual who embraces their flaws and knows they are awesome regardless",
+            "Pretending to work when you are actually not doing anything",
+            "When you are unable to type without repeatedly making mistakes",
+            "Someone who speaks confidently on a subject of which they know absolutely nothing",
+            "Drowsiness after eating a large meal",
+            "The anticipation felt when waiting for a response to a text"
+    };
+    public static String[] shuffleAndPick(){
+        String[] word = new String[2];
+        int num = (int) (Math.random()*weirdWord.length);
+        word[0] = weirdWord[num];
+        word[1] = weirdMeaning[num];
+        return word;
+    }
+}
